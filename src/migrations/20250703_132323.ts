7ef379fc7d1d274b8ffca458fe036e18bcbb5ae2
Lautaro Amengual <amengual.ln@gmail.com>
2025-07-03 10:25:08 -0300

    CMS migrate

---
+++ src/migrations/20250703_132323.ts
@@ -0,0 +1,293 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_careers_status" AS ENUM('active', 'closed');
+  CREATE TABLE "posts_tags" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"tag" varchar
+  );
+  
+  CREATE TABLE "careers_requirements" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"requirement" varchar
+  );
+  
+  CREATE TABLE "careers" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"ref" varchar,
+  	"position" varchar,
+  	"area" varchar,
+  	"company" varchar,
+  	"description" jsonb,
+  	"location" varchar,
+  	"salary" varchar,
+  	"linked_in" varchar,
+  	"status" "enum_careers_status",
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  ALTER TABLE "posts_populated_authors" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "posts_rels" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "_posts_v_version_populated_authors" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "_posts_v" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "_posts_v_rels" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "header_nav_items" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "header" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "header_rels" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "footer_nav_items" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "footer" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "footer_rels" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "posts_populated_authors" CASCADE;
+  DROP TABLE "posts_rels" CASCADE;
+  DROP TABLE "_posts_v_version_populated_authors" CASCADE;
+  DROP TABLE "_posts_v" CASCADE;
+  DROP TABLE "_posts_v_rels" CASCADE;
+  DROP TABLE "header_nav_items" CASCADE;
+  DROP TABLE "header" CASCADE;
+  DROP TABLE "header_rels" CASCADE;
+  DROP TABLE "footer_nav_items" CASCADE;
+  DROP TABLE "footer" CASCADE;
+  DROP TABLE "footer_rels" CASCADE;
+  ALTER TABLE "posts" DROP CONSTRAINT "posts_hero_image_id_media_id_fk";
+  
+  ALTER TABLE "posts" DROP CONSTRAINT "posts_meta_image_id_media_id_fk";
+  
+  DROP INDEX "posts_hero_image_idx";
+  DROP INDEX "posts_meta_meta_image_idx";
+  DROP INDEX "posts_slug_idx";
+  DROP INDEX "posts__status_idx";
+  ALTER TABLE "posts" ADD COLUMN "excerpt" varchar;
+  ALTER TABLE "posts" ADD COLUMN "image" varchar;
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "careers_id" integer;
+  ALTER TABLE "posts_tags" ADD CONSTRAINT "posts_tags_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "careers_requirements" ADD CONSTRAINT "careers_requirements_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."careers"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "posts_tags_order_idx" ON "posts_tags" USING btree ("_order");
+  CREATE INDEX "posts_tags_parent_id_idx" ON "posts_tags" USING btree ("_parent_id");
+  CREATE INDEX "careers_requirements_order_idx" ON "careers_requirements" USING btree ("_order");
+  CREATE INDEX "careers_requirements_parent_id_idx" ON "careers_requirements" USING btree ("_parent_id");
+  CREATE INDEX "careers_updated_at_idx" ON "careers" USING btree ("updated_at");
+  CREATE INDEX "careers_created_at_idx" ON "careers" USING btree ("created_at");
+  ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_careers_fk" FOREIGN KEY ("careers_id") REFERENCES "public"."careers"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "payload_locked_documents_rels_careers_id_idx" ON "payload_locked_documents_rels" USING btree ("careers_id");
+  ALTER TABLE "posts" DROP COLUMN "hero_image_id";
+  ALTER TABLE "posts" DROP COLUMN "meta_title";
+  ALTER TABLE "posts" DROP COLUMN "meta_image_id";
+  ALTER TABLE "posts" DROP COLUMN "meta_description";
+  ALTER TABLE "posts" DROP COLUMN "published_at";
+  ALTER TABLE "posts" DROP COLUMN "slug";
+  ALTER TABLE "posts" DROP COLUMN "slug_lock";
+  ALTER TABLE "posts" DROP COLUMN "_status";
+  DROP TYPE "public"."enum_posts_status";
+  DROP TYPE "public"."enum__posts_v_version_status";
+  DROP TYPE "public"."enum_header_nav_items_link_type";
+  DROP TYPE "public"."enum_footer_nav_items_link_type";`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE TYPE "public"."enum_posts_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum__posts_v_version_status" AS ENUM('draft', 'published');
+  CREATE TYPE "public"."enum_header_nav_items_link_type" AS ENUM('reference', 'custom');
+  CREATE TYPE "public"."enum_footer_nav_items_link_type" AS ENUM('reference', 'custom');
+  CREATE TABLE "posts_populated_authors" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar
+  );
+  
+  CREATE TABLE "posts_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"posts_id" integer,
+  	"categories_id" integer,
+  	"users_id" integer
+  );
+  
+  CREATE TABLE "_posts_v_version_populated_authors" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"_uuid" varchar,
+  	"name" varchar
+  );
+  
+  CREATE TABLE "_posts_v" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"parent_id" integer,
+  	"version_title" varchar,
+  	"version_hero_image_id" integer,
+  	"version_content" jsonb,
+  	"version_meta_title" varchar,
+  	"version_meta_image_id" integer,
+  	"version_meta_description" varchar,
+  	"version_published_at" timestamp(3) with time zone,
+  	"version_slug" varchar,
+  	"version_slug_lock" boolean DEFAULT true,
+  	"version_updated_at" timestamp(3) with time zone,
+  	"version_created_at" timestamp(3) with time zone,
+  	"version__status" "enum__posts_v_version_status" DEFAULT 'draft',
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"latest" boolean,
+  	"autosave" boolean
+  );
+  
+  CREATE TABLE "_posts_v_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"posts_id" integer,
+  	"categories_id" integer,
+  	"users_id" integer
+  );
+  
+  CREATE TABLE "header_nav_items" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"link_type" "enum_header_nav_items_link_type" DEFAULT 'reference',
+  	"link_new_tab" boolean,
+  	"link_url" varchar,
+  	"link_label" varchar NOT NULL
+  );
+  
+  CREATE TABLE "header" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
+  
+  CREATE TABLE "header_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"pages_id" integer,
+  	"posts_id" integer
+  );
+  
+  CREATE TABLE "footer_nav_items" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"link_type" "enum_footer_nav_items_link_type" DEFAULT 'reference',
+  	"link_new_tab" boolean,
+  	"link_url" varchar,
+  	"link_label" varchar NOT NULL
+  );
+  
+  CREATE TABLE "footer" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
+  
+  CREATE TABLE "footer_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"pages_id" integer,
+  	"posts_id" integer
+  );
+  
+  ALTER TABLE "posts_tags" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "careers_requirements" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "careers" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "posts_tags" CASCADE;
+  DROP TABLE "careers_requirements" CASCADE;
+  DROP TABLE "careers" CASCADE;
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_careers_fk";
+  
+  DROP INDEX "payload_locked_documents_rels_careers_id_idx";
+  ALTER TABLE "posts" ADD COLUMN "hero_image_id" integer;
+  ALTER TABLE "posts" ADD COLUMN "meta_title" varchar;
+  ALTER TABLE "posts" ADD COLUMN "meta_image_id" integer;
+  ALTER TABLE "posts" ADD COLUMN "meta_description" varchar;
+  ALTER TABLE "posts" ADD COLUMN "published_at" timestamp(3) with time zone;
+  ALTER TABLE "posts" ADD COLUMN "slug" varchar;
+  ALTER TABLE "posts" ADD COLUMN "slug_lock" boolean DEFAULT true;
+  ALTER TABLE "posts" ADD COLUMN "_status" "enum_posts_status" DEFAULT 'draft';
+  ALTER TABLE "posts_populated_authors" ADD CONSTRAINT "posts_populated_authors_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "posts_rels" ADD CONSTRAINT "posts_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "posts_rels" ADD CONSTRAINT "posts_rels_posts_fk" FOREIGN KEY ("posts_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "posts_rels" ADD CONSTRAINT "posts_rels_categories_fk" FOREIGN KEY ("categories_id") REFERENCES "public"."categories"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "posts_rels" ADD CONSTRAINT "posts_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_posts_v_version_populated_authors" ADD CONSTRAINT "_posts_v_version_populated_authors_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."_posts_v"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_posts_v" ADD CONSTRAINT "_posts_v_parent_id_posts_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."posts"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_posts_v" ADD CONSTRAINT "_posts_v_version_hero_image_id_media_id_fk" FOREIGN KEY ("version_hero_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_posts_v" ADD CONSTRAINT "_posts_v_version_meta_image_id_media_id_fk" FOREIGN KEY ("version_meta_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "_posts_v_rels" ADD CONSTRAINT "_posts_v_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."_posts_v"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_posts_v_rels" ADD CONSTRAINT "_posts_v_rels_posts_fk" FOREIGN KEY ("posts_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_posts_v_rels" ADD CONSTRAINT "_posts_v_rels_categories_fk" FOREIGN KEY ("categories_id") REFERENCES "public"."categories"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "_posts_v_rels" ADD CONSTRAINT "_posts_v_rels_users_fk" FOREIGN KEY ("users_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "header_nav_items" ADD CONSTRAINT "header_nav_items_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."header"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "header_rels" ADD CONSTRAINT "header_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."header"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "header_rels" ADD CONSTRAINT "header_rels_pages_fk" FOREIGN KEY ("pages_id") REFERENCES "public"."pages"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "header_rels" ADD CONSTRAINT "header_rels_posts_fk" FOREIGN KEY ("posts_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "footer_nav_items" ADD CONSTRAINT "footer_nav_items_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."footer"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "footer_rels" ADD CONSTRAINT "footer_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."footer"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "footer_rels" ADD CONSTRAINT "footer_rels_pages_fk" FOREIGN KEY ("pages_id") REFERENCES "public"."pages"("id") ON DELETE cascade ON UPDATE no action;
+  ALTER TABLE "footer_rels" ADD CONSTRAINT "footer_rels_posts_fk" FOREIGN KEY ("posts_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "posts_populated_authors_order_idx" ON "posts_populated_authors" USING btree ("_order");
+  CREATE INDEX "posts_populated_authors_parent_id_idx" ON "posts_populated_authors" USING btree ("_parent_id");
+  CREATE INDEX "posts_rels_order_idx" ON "posts_rels" USING btree ("order");
+  CREATE INDEX "posts_rels_parent_idx" ON "posts_rels" USING btree ("parent_id");
+  CREATE INDEX "posts_rels_path_idx" ON "posts_rels" USING btree ("path");
+  CREATE INDEX "posts_rels_posts_id_idx" ON "posts_rels" USING btree ("posts_id");
+  CREATE INDEX "posts_rels_categories_id_idx" ON "posts_rels" USING btree ("categories_id");
+  CREATE INDEX "posts_rels_users_id_idx" ON "posts_rels" USING btree ("users_id");
+  CREATE INDEX "_posts_v_version_populated_authors_order_idx" ON "_posts_v_version_populated_authors" USING btree ("_order");
+  CREATE INDEX "_posts_v_version_populated_authors_parent_id_idx" ON "_posts_v_version_populated_authors" USING btree ("_parent_id");
+  CREATE INDEX "_posts_v_parent_idx" ON "_posts_v" USING btree ("parent_id");
+  CREATE INDEX "_posts_v_version_version_hero_image_idx" ON "_posts_v" USING btree ("version_hero_image_id");
+  CREATE INDEX "_posts_v_version_meta_version_meta_image_idx" ON "_posts_v" USING btree ("version_meta_image_id");
+  CREATE INDEX "_posts_v_version_version_slug_idx" ON "_posts_v" USING btree ("version_slug");
+  CREATE INDEX "_posts_v_version_version_updated_at_idx" ON "_posts_v" USING btree ("version_updated_at");
+  CREATE INDEX "_posts_v_version_version_created_at_idx" ON "_posts_v" USING btree ("version_created_at");
+  CREATE INDEX "_posts_v_version_version__status_idx" ON "_posts_v" USING btree ("version__status");
+  CREATE INDEX "_posts_v_created_at_idx" ON "_posts_v" USING btree ("created_at");
+  CREATE INDEX "_posts_v_updated_at_idx" ON "_posts_v" USING btree ("updated_at");
+  CREATE INDEX "_posts_v_latest_idx" ON "_posts_v" USING btree ("latest");
+  CREATE INDEX "_posts_v_autosave_idx" ON "_posts_v" USING btree ("autosave");
+  CREATE INDEX "_posts_v_rels_order_idx" ON "_posts_v_rels" USING btree ("order");
+  CREATE INDEX "_posts_v_rels_parent_idx" ON "_posts_v_rels" USING btree ("parent_id");
+  CREATE INDEX "_posts_v_rels_path_idx" ON "_posts_v_rels" USING btree ("path");
+  CREATE INDEX "_posts_v_rels_posts_id_idx" ON "_posts_v_rels" USING btree ("posts_id");
+  CREATE INDEX "_posts_v_rels_categories_id_idx" ON "_posts_v_rels" USING btree ("categories_id");
+  CREATE INDEX "_posts_v_rels_users_id_idx" ON "_posts_v_rels" USING btree ("users_id");
+  CREATE INDEX "header_nav_items_order_idx" ON "header_nav_items" USING btree ("_order");
+  CREATE INDEX "header_nav_items_parent_id_idx" ON "header_nav_items" USING btree ("_parent_id");
+  CREATE INDEX "header_rels_order_idx" ON "header_rels" USING btree ("order");
+  CREATE INDEX "header_rels_parent_idx" ON "header_rels" USING btree ("parent_id");
+  CREATE INDEX "header_rels_path_idx" ON "header_rels" USING btree ("path");
+  CREATE INDEX "header_rels_pages_id_idx" ON "header_rels" USING btree ("pages_id");
+  CREATE INDEX "header_rels_posts_id_idx" ON "header_rels" USING btree ("posts_id");
+  CREATE INDEX "footer_nav_items_order_idx" ON "footer_nav_items" USING btree ("_order");
+  CREATE INDEX "footer_nav_items_parent_id_idx" ON "footer_nav_items" USING btree ("_parent_id");
+  CREATE INDEX "footer_rels_order_idx" ON "footer_rels" USING btree ("order");
+  CREATE INDEX "footer_rels_parent_idx" ON "footer_rels" USING btree ("parent_id");
+  CREATE INDEX "footer_rels_path_idx" ON "footer_rels" USING btree ("path");
+  CREATE INDEX "footer_rels_pages_id_idx" ON "footer_rels" USING btree ("pages_id");
+  CREATE INDEX "footer_rels_posts_id_idx" ON "footer_rels" USING btree ("posts_id");
+  ALTER TABLE "posts" ADD CONSTRAINT "posts_hero_image_id_media_id_fk" FOREIGN KEY ("hero_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  ALTER TABLE "posts" ADD CONSTRAINT "posts_meta_image_id_media_id_fk" FOREIGN KEY ("meta_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  CREATE INDEX "posts_hero_image_idx" ON "posts" USING btree ("hero_image_id");
+  CREATE INDEX "posts_meta_meta_image_idx" ON "posts" USING btree ("meta_image_id");
+  CREATE INDEX "posts_slug_idx" ON "posts" USING btree ("slug");
+  CREATE INDEX "posts__status_idx" ON "posts" USING btree ("_status");
+  ALTER TABLE "posts" DROP COLUMN "excerpt";
+  ALTER TABLE "posts" DROP COLUMN "image";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN "careers_id";
+  DROP TYPE "public"."enum_careers_status";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250629_202644_initial from './20250629_202644_initial'
+import * as migration_20250629_202644_initial from './20250629_202644_initial';
+import * as migration_20250703_132323 from './20250703_132323';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250629_202644_initial.down,
     name: '20250629_202644_initial',
   },
-]
+  {
+    up: migration_20250703_132323.up,
+    down: migration_20250703_132323.down,
+    name: '20250703_132323'
+  },
+];
