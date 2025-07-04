ca3d7630f223059f17eb91d2bd7eb6cbe54c8a53
Lautaro Amengual <amengual.ln@gmail.com>
2025-07-04 09:57:43 -0300

    clean migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250629_202644_initial from './20250629_202644_initial';
 import * as migration_20250703_132323 from './20250703_132323';
+import * as migration_20250704_125644 from './20250704_125644';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250703_132323.up,
     down: migration_20250703_132323.down,
-    name: '20250703_132323'
+    name: '20250703_132323',
+  },
+  {
+    up: migration_20250704_125644.up,
+    down: migration_20250704_125644.down,
+    name: '20250704_125644'
   },
 ];
